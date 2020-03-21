7ad5a7c0a62b5d11e756478ea43e7e2a40d8c2ac
mluc <my.luc@opcity.com>
2020-03-21 16:35:58 -0500

    working

--- eth-contracts/test/TestSquareVerifier.js
+++ eth-contracts/test/TestSquareVerifier.js
@@ -10,3 +10,38 @@
 
     
 // Test verification with incorrect proof
+
+
+var Test = require('../config/testConfig.js');
+const  truffleAssert = require('truffle-assertions')
+
+contract('TestSquareVerifier', accounts => {
+
+    describe('Test SquareVerifier', function () {
+        var config;
+
+        beforeEach(async function () {
+            config = await Test.Config(accounts);
+
+        })
+
+        it('Test verification with correct proof', async function () {
+            let result =  await config.verifier.verifyTx(config.params.proof.a, config.params.proof.b, config.params.proof.c, config.params.inputs);
+            truffleAssert.eventEmitted(result, "Verified");
+            assert.equal(result.logs[0].args.s, 'Transaction successfully verified.')
+
+            result =  await config.verifier.verifyTx.call(config.params.proof.a, config.params.proof.b, config.params.proof.c, config.params.inputs);
+            assert.equal(result, true);
+        })
+
+        it('Test verification with incorrect proof', async function () {
+            const input = ["0x0000000000000000000000000000000000000000000000000000000000000008", "0x0000000000000000000000000000000000000000000000000000000000000001"];
+            let result =  await config.verifier.verifyTx.call(config.params.proof.a, config.params.proof.b, config.params.proof.c, input);
+            assert.equal(result, false);
+        })
+
+    })
+})
+
+
+
